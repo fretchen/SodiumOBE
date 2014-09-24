41fad1733c429b2c76150ce2552fe2dd3caba06d
Fred Jendrzejewski <fred.jendrzejewski@gmail.com>
2014-09-24 13:33:25 -0400

    add some normalizations
    Allows now to normalize the Coupling constant and the decay amplitude
    by some terms.

--- DecayAmplitude.m
+++ DecayAmplitude.m
@@ -1,6 +1,23 @@
-function Gamma = DecayAmplitude(F,Fp,mF,mFp)
+function Gamma = DecayAmplitude(varargin)
+p = inputParser;
+
+p.addRequired('F', @(x)isnumeric(x));
+p.addRequired('Fp', @(x)isnumeric(x));
+p.addRequired('mF', @(x)isnumeric(x));
+p.addRequired('mFp', @(x)isnumeric(x));
+
+p.addOptional('dMEnorm', 1, @(x)isnumeric(x));
+
+parse(p,varargin{:});
+
+F = p.Results.F;
+Fp = p.Results.Fp;
+mF = p.Results.mF;
+mFp = p.Results.mFp;
+
+dMEnorm = p.Results.dMEnorm;
 
 Gamma =0;
 for q = -1:1
-    Gamma = Gamma+dipoleMatrixEl(F,Fp,mF,mFp,q).^2;
+    Gamma = Gamma+dipoleMatrixEl(F,Fp,mF,mFp,q).^2./dMEnorm.^2;
 end
--- getCouplingForAllPolarizations.m
+++ getCouplingForAllPolarizations.m
@@ -1,5 +1,27 @@
-function C = getCouplingForAllPolarizations(Elf, F,Fp, mF,mFp)
+function C = getCouplingForAllPolarizations(varargin)
+%% calaculate the coupling strength of the electric field between the 
+%% specific states
+p = inputParser;
+
+p.addRequired('Elf',@(x)isnumeric(x));
+p.addRequired('F', @(x)isnumeric(x));
+p.addRequired('Fp', @(x)isnumeric(x));
+p.addRequired('mF', @(x)isnumeric(x));
+p.addRequired('mFp', @(x)isnumeric(x));
+
+p.addOptional('dMEnorm', 1, @(x)isnumeric(x));
+
+parse(p,varargin{:});
+
 C = 0;
+Elf = p.Results.Elf;
+F = p.Results.F;
+Fp = p.Results.Fp;
+mF = p.Results.mF;
+mFp = p.Results.mFp;
+
+dMEnorm = p.Results.dMEnorm;
+
 for q = -1:1
-    C = C+ Elf(q+2)*dipoleMatrixEl(F,Fp,mF,mFp,q);
+    C = C+ Elf(q+2)*dipoleMatrixEl(F,Fp,mF,mFp,q)./dMEnorm;
 end
